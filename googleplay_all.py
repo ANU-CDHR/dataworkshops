5bc560e56c500b73786c949a79e50fc9e6cd1310
junranlei <junran.lei@anu.edu.au>
2021-01-22 01:38:11 +0000

    change header/links, add examples/overviews/text, change codes

--- googleplay_all.py
+++ googleplay_all.py
@@ -1,4 +1,4 @@
-"""Python file for whole data set process, to generate chart in html file. Be mindful that it may take a long time and occupy much space depends on the number of reviews for the requested Google play app. Eg, Whatsapp return around 7 millions reviews and 2GB data, the generated chart html for histogram chart for review score and date is over 320MB."""
+"""Python file for whole data set process, to generate chart in html file. Be mindful that it may take a long time and occupy much space depends on the number of reviews for the requested Google play app. Eg, Whatsapp return around 12 millions reviews and 3GB data, the generated chart html for histogram chart for review score and date is over 320MB."""
 #Import required libraries
 import pandas as pd
 import glob, os 
@@ -21,4 +21,4 @@ import plotly.express as px
 
 # Histogram chart for Score, thumbsupcount data counts
 fig = px.histogram(df_all, x='at', nbins=15, color="score",marginal='box',barmode='group',category_orders={"score":[5,4,3,2,1]})
-plotly.offline.plot(fig, filename='3.4.histogram_all_01.html')
+plotly.offline.plot(fig, filename='3.2.histogram_all_01.html')
